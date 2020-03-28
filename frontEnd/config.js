911d025241b4797660b95b25ca6bd8a57aca0392
Holegots <>
2020-03-29 04:37:28 +0800

    feat: add Load balancing

--- frontEnd/config.js
+++ frontEnd/config.js
@@ -1,1 +1,4 @@
-export default 'https://service-bitqtpbh-1257685189.bj.apigw.tencentcs.com/release/getDouban'
+const urls=[
+  'https://service-bitqtpbh-1257685189.bj.apigw.tencentcs.com/release/getDouban'
+]
+export default urls[Math.floor(Math.random() * urls.length)];
